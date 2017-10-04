5047da49478377250a5ccc72f9fef461f4756ae2
madqiyud <qiyuddin@gmail.com>
2017-10-04 15:00:06 +0800

    ok saya da uload kod saya ;-))
    projek KPD202/JPJ

---
+++ 10Julai/latihan2.java
@@ -0,0 +1,7 @@
+class Latihan2
+{
+	public static void main(String args[])
+	{
+		System.out.println("Susur 7, \n Jalan Tun Andul Razak");
+	}
+}
---
+++ 10Julai/latihan5.java
@@ -0,0 +1,4 @@
+Class Latihan5
+{
+public Static void main(string args[]){system.out.println("welcome");}
+}
